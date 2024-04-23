afe027cb11ee9276775f43c87efa05ba6cac29cb
alvaro valencia <alvaroleandro74@gmail.com>
2024-04-23 01:32:05 -0300

    Updates

--- static/media/correo.2b66351c1887197ec34c.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $para      = 'alvaroleandro74@gmail.com';
-    $titulo    = 'El título';
-    $mensaje   = 'Hola';
-
-
-    mail($para, $titulo, $mensaje);
-    echo '<h1> se esta ejecutando mi codigo php </h1>';
-?>
